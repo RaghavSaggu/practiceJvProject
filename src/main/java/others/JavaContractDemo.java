4c6a7765566a0a37785a04aaabe71df2179fe5d2
Raghav <raghav@twinline.in>
2024-09-01 11:55:29 +0530

    java object contract must be fulfilled

--- src/main/java/others/JavaContractDemo.java
+++ src/main/java/others/JavaContractDemo.java
@@ -1,14 +1,15 @@
-package demo;
+package others;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
+import java.util.Map;
 
-public class Test {
+public class JavaContractDemo {
     public static void main(String[] args) {
         // main method
-        List<Student> studentList = Arrays.asList(new Student(8, "Raghav"),
+        List<Student> studentList = Arrays.asList(
+                new Student(8, "Raghav"),
                 new Student(2, "Karan"),
                 new Student(2, "Rupinder"),
                 new Student(1, "Pardeep"),
@@ -17,21 +18,28 @@ public class Test {
                 new Student(5, "Japjeet"),
                 new Student(6, "Pushpinder"),
                 new Student(7, "Kewal"));
-//        studentList.stream()
-//                .sorted((o1, o2) -> o2.getId().compareTo(o1.getId()))
-////                .sorted()
-//                .forEach(integer -> System.out.println(integer));
-        studentList.get(0).show();
+        studentList.stream()
+                .sorted((o1, o2) -> o2.getId().compareTo(o1.getId()))
+//                .sorted()
+                .forEach(integer -> System.out.println(integer));
+
+        Student obj1 = new Student(8, "Raghav");
+        Student obj2 = new Student(8, "Raghav");
+        System.out.println("equals using == operator: " + (obj1 == obj2));
+        System.out.println("equals using equals method: " + obj1.equals(obj2));
+        System.out.println("Hash code of obj1: " + obj1.hashCode());
+        System.out.println("Hash code of obj2: " + obj2.hashCode());
+
+        Map<Student, Student> map = new HashMap<>();
+        map.put(obj1, obj1);
+        map.put(obj2, obj2);
+        for(Map.Entry<Student, Student> entry: map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + map.values());
+        }
     }
 }
 
-class Person {
-    void show() throws NullPointerException {
-        System.out.println("I am person");
-    }
-}
-
-class Student extends Person implements Comparable {
+class Student implements Comparable {
     Integer id;
     String name;
 
@@ -56,6 +64,9 @@ class Student extends Person implements Comparable {
         this.name = name;
     }
 
+    /* java contract says if we will override equals, we must override hascode too;
+     * check by commenting overriden hashcode method
+     * */
     @Override
     public int hashCode() {
         return (31 * 1) + this.id;
@@ -65,6 +76,7 @@ class Student extends Person implements Comparable {
     public boolean equals(Object obj) {
         return this.id.compareTo(((Student)obj).id) == 0;
     }
+    // java contract ends here
 
     @Override
     public String toString() {
@@ -77,10 +89,5 @@ class Student extends Person implements Comparable {
     @Override
     public int compareTo(Object o) {
         return this.id.compareTo(((Student)o).id) != 0 ? this.id.compareTo(((Student)o).id) : this.name.compareTo(((Student) o).name);
-    }
-
-    @Override
-    void show() throws RuntimeException {
-        System.out.println("I am a student as a person");;
     }
 }
