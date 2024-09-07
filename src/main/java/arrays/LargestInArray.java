cf8343a2740183d3cce2cbf213ff9e9d9d2aa4f4
Raghav <raghav@twinline.in>
2024-09-07 23:56:00 +0530

    Wissen Tech test

--- src/main/java/arrays/LargestInArray.java
+++ src/main/java/arrays/LargestInArray.java
@@ -1,23 +1,37 @@
 package arrays;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LargestInArray {
     public static void main(String[] args) {
         int[] arr = {91, 3, 511, 7, 9};
-        int largest = arr[0];
-//        for (int num : arr) {
-//            if (num > largest) {
-//                largest = num;
-//            }
-//        }
-
-//        for (int i = 0; i < arr.length; i++) {
-//            if (arr[i] > largest)
-//                largest = arr[i];
-//        }
-        Arrays.sort(arr);
-        largest = arr[arr.length - 1];
+        int largest = largestFromArrayUsingStream(arr);
         System.out.println("Largest in array is: " + largest);
+    }
+
+    private static int largestFromArrayUsingStream(int[] arr) {
+        List<Integer> integerList = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        int largest = integerList.stream().max((o1, o2) -> Integer.compare(o1, o2)).orElseThrow();
+        return largest;
+    }
+
+    private static int largestFromArray(int[] arr) {
+        int largest = 0;
+        if (arr.length > 0) {
+            Arrays.sort(arr);
+            largest = arr[arr.length - 1];
+        }
+        return largest;
+    }
+
+    private static int largestFromArrayUsingBruteForce(int[] arr) {
+        int largest = 0;
+        for (int num : arr)
+            largest = num > largest ? num : largest;
+//        for (int i = 0; i < arr.length; i++)
+//            largest = arr[i] > largest ? arr[i] : largest;
+        return largest;
     }
 }
---
+++ src/main/java/arrays/MinMaxFromIntegers.java
@@ -0,0 +1,39 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MinMaxFromIntegers {
+    public static void main(String[] args) {
+        int[] intArr = {5, 4, 3, 2, 1};
+        printMinAndMaxUsingBruteForce(intArr);
+//        printMinAndMaxUsingStreamMinMax(intArr);
+//        printMinAndMaxUsingStream(intArr);
+    }
+
+    private static void printMinAndMaxUsingBruteForce(int[] intArr) {
+        Arrays.sort(intArr);
+        System.out.println("Brute Force Way");
+        System.out.print("Min was : " + intArr[0]);
+        System.out.println(", Max was : " + intArr[intArr.length - 1]);
+    }
+
+    // another way
+    private static void printMinAndMaxUsingStreamMinMax(int[] intArr) {
+        List<Integer> intList = Arrays.stream(intArr).boxed().collect(Collectors.toList());
+        System.out.println("Using stream min and max");
+        System.out.print("Min was : " + intList.stream().min(Integer::compare).get());
+        System.out.println(", Max was : " + intList.stream().max(Integer::compare).get());
+    }
+
+    // another way
+    private static void printMinAndMaxUsingStream(int[] intArr) {
+        List<Integer> intList = Arrays.stream(intArr).boxed().collect(Collectors.toList());
+        List<Integer> sortedList = intList.stream().sorted().collect(Collectors.toList());
+        System.out.println("Using stream sorting");
+        System.out.print("Min was : " + sortedList.get(0));
+        System.out.println(", Max was : " + sortedList.get(sortedList.size() - 1));
+
+    }
+}
