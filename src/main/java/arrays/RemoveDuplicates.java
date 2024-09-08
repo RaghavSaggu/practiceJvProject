3d58ae1c50665911794d3403ce82319ea0459b9d
Raghav <raghav@twinline.in>
2024-09-08 09:41:45 +0530

    remove duplicate from int array

--- src/main/java/arrays/RemoveDuplicates.java
+++ src/main/java/arrays/RemoveDuplicates.java
@@ -6,20 +6,30 @@ import java.util.stream.Collectors;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 2, 3, 4, 4};
-//        arr = removeDuplicateUsingBruteForce(arr);
+        int[] arr = {0, 1, 2, 2, 3, 4, 4, 0, -5, -6, -5, -1, 1};
+        arr = removeDuplicateUsingBruteForce(arr);
 //        arr = removeDuplicateUsingMap(arr);
-        arr = removeDuplicateUsingStream(arr);
-        System.out.println();
+//        arr = removeDuplicateUsingStream(arr);
+        Arrays.stream(arr).sorted().forEach(elem -> System.out.print(elem + " "));
     }
 
     private static int[] removeDuplicateUsingBruteForce(int[] arr) {
         if(arr == null || arr.length <= 0) return null;
-        int[] newArr = new int[arr.length];
-        for(int i = 0, j = 0; i < arr.length; i++) {
-            final int streamVar = i;
-            if(Arrays.stream(newArr).filter(n -> n == arr[streamVar]).count() == 0) {
-                newArr[j] = arr[i];
+        int newSize = 0;
+        if(Arrays.stream(arr).filter(n -> n == 0).count() > 0)
+            newSize++;
+        int[] arrToGetNewSize = new int[arr.length];
+        for(int i = 0, j = 0; i < arr.length; i++, j++) {
+            final int streamVar = arr[i];
+            if(Arrays.stream(arrToGetNewSize).filter(n -> n == streamVar).count() == 0) {
+                arrToGetNewSize[j] = streamVar;
+                newSize++;
+            }
+        }
+        int newArr[] = new int[newSize];
+        for(int i = 0, j = 0; i < arrToGetNewSize.length && j < newSize; i++) {
+            if(arrToGetNewSize[i] != 0) {
+                newArr[j] = arrToGetNewSize[i];
                 j++;
             }
         }
@@ -39,7 +49,6 @@ public class RemoveDuplicates {
         if(arr == null || arr.length <= 0) return null;
         HashSet<Integer> set = new HashSet<>();
         set = (HashSet<Integer>) Arrays.stream(arr).boxed().collect(Collectors.toSet());
-        System.out.println(set);
         return set.stream().mapToInt(Integer::intValue).toArray();
     }
 }
