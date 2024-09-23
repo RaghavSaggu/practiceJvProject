713dccf6d9efedc7c2ee38d5db31740423a90391
Raghav <raghav@twinline.in>
2024-09-23 18:13:38 +0530

    stream flow and collectors understanding

--- src/main/java/collectionAndStreams/StreamInternalWorking.java
+++ src/main/java/collectionAndStreams/StreamInternalWorking.java
@@ -2,13 +2,14 @@ package collectionAndStreams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class StreamInternalWorking {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1,2,3,4,5);
         StreamInternalWorking stream = new StreamInternalWorking();
-        stream.streamDemoReduce(list);
+        stream.streamDemoCollectorsFiltering(list);
     }
 
     private void streamDemo(List<Integer> list) {
@@ -59,6 +60,51 @@ public class StreamInternalWorking {
                     System.out.println("C + E :" + c + " + " + e);
                     return c + e;
                 })));
+    }
+
+    private void streamDemoCollectorsSummarizingInt(List<Integer> list) {
+        System.out.println("Answer is: " + list.stream()
+                .peek(i -> System.out.println("Peeking Value: " + i))
+                .collect(Collectors.summarizingInt(value -> {
+                    System.out.println("Summarizing: " + value);
+                    return value;
+                })));
+    }
+
+    private void streamDemoCollectorsGroupingBy(List<Integer> list) {
+        System.out.println("Answer is: " + list.stream()
+                .peek(i -> System.out.println("Peeking Value: " + i))
+                .collect(Collectors.groupingBy(value -> {
+                    System.out.println("Grouping By: " + value);
+                    return value;
+                })));
+    }
+
+    private void streamDemoCollectorsGroupingBy2Param(List<Integer> list) {
+        System.out.println("Answer is: " + list.stream()
+                .peek(i -> System.out.println("Peeking Value: " + i))
+                .collect(Collectors.groupingBy(value -> {
+                    System.out.println("Grouping By: " + value);
+                    return value;
+                }, Collectors.counting())));
+    }
+
+    private void streamDemoCollectorsGroupingBy3Param(List<Integer> list) {
+        System.out.println("Answer is: " + list.stream()
+                .peek(i -> System.out.println("Peeking Value: " + i))
+                .collect(Collectors.groupingBy(value -> {
+                    System.out.println("Grouping By: " + value);
+                    return value;
+                }, TreeMap::new, Collectors.counting())));
+    }
+
+    private void streamDemoCollectorsFiltering(List<Integer> list) {
+        System.out.println("Answer is: " + list.stream()
+                .peek(i -> System.out.println("Peeking Value: " + i))
+                .collect(Collectors.filtering(i -> {
+                    System.out.println("Filtering using Collectors: " + i);
+                    return i % 2 == 0;
+                }, Collectors.counting())));
     }
 
     private void parallelStreamDemo(List<Integer> list) {
