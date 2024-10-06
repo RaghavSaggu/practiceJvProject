bf00e0dbb255b8ffcbcea805d56b06cff5b7a422
Raghav <raghav@twinline.in>
2024-10-06 22:21:51 +0530

    minor change

--- src/main/java/stringRelated/PrintNonNullStrings.java
+++ src/main/java/stringRelated/PrintNonNullStrings.java
@@ -9,7 +9,6 @@ public class PrintNonNullStrings {
     public static void main(String[] args) {
         //Write a Java program to filter out null values from a list of strings.
         List<String> words = Arrays.asList("apple", null, "banana", null, "cat");
-        words = words.stream().filter(Objects::nonNull).collect(Collectors.toList());
-        System.out.println(words);
+        words.stream().filter(Objects::nonNull).forEach(System.out::println);
     }
 }
